4f8c43ab67ea54a682dd1d820845b96ba839bb51
Song Cho <iamsongcho@gmail.com>
2022-03-12 19:24:38 +0900

    add linear gradient bar on the top

--- src/screens/Resume/index.js
+++ src/screens/Resume/index.js
@@ -1,25 +1,34 @@
 import React from "react";
 import "./styles.css";
 
-const Resume = (props) => {
-  console.log(props);
-  console.log("sdlkfjsldfjlskdfjlskdfjlskdjflsk");
+const TitleBar = () => {
   return (
-    <div className="resume">
-      <p className="resume-title">
-        Hello! <br /> I'm Song Cho<span className="period__mark">.</span>
-      </p>
+    <div
+      style={{
+        backgroundImage: "linear-gradient(to right, #4880EC, #39d2fd)",
+        height: 10,
+      }}
+    />
+  );
+};
 
-      <p className="resume-intro">
-        Specialized in AWS, React & React Native and eager to become a
-        professional solution architect. 5+ years of experience as a Backend
-        Engineer and 3+ years of experience as a react native developer.
-        Self-driven, passionate about learning new skills and believe that
-        leveraging technology is best way to create social impact. In my free
-        time, I often enjoy playing drums and travel around to experience new
-        culture.
-        <br />
-      </p>
+const Resume = (props) => {
+  return (
+    <div>
+      <TitleBar />
+      <div className="resume__wrapper">
+        <p className="resume-title">
+          Hello! <br /> I'm Song Cho<span className="period__mark">.</span>
+        </p>
+
+        <p className="resume-intro">
+          Self-driven, passionate about learning new skills and believe that
+          leveraging technology is best way to create social impact. In my free
+          time, I often enjoy playing drums and travel around to experience new
+          culture.
+          <br />
+        </p>
+      </div>
     </div>
   );
 };
